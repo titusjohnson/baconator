1d65a38608a28cb37e9c126fb39994757dcc4fa3
Titus Johnson <titus@simplepixel.org>
2016-04-14 22:55:55 -0500

    Adds a bunch of gems
    Guard because I'm not a savage
    Ruby Tree because I don't want to write a tree
    Webmock & VCR because I don't want to get banned from Wiki

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require 'vcr'
+
+Dir[File.dirname(__FILE__) + '/../lib/*'].each {|file| require file }
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock # or :fakeweb
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
