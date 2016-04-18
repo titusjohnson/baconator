009ab8c66897e9eac3dbb4d7240b3c1037a29cca
Titus Johnson <titus@simplepixel.org>
2016-04-17 20:45:58 -0500

    Overhauls the scraper class, moving logic off of the Baconator
    Probably don't even need a class for this, I tossed out the state handling (dumb idea anyway, don't need state). I'm leaving it as a class because I spent too much time mucking about with it, and it's working fine now.
    
    Adds some more tests

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,23 +6,63 @@ class Scraper
 
   WIKIPEDIA_URL = 'https://en.wikipedia.org'
 
-  def initialize
-    reset
+  # 
+  # Given a page, fetch out a unique list of outbound links
+  # 
+  # @param page [String] a /wiki/Page_URL
+  # 
+  # @return [Array]
+  def find_unique_linked_pages(page)
+    links = scrape_page_for_links page
+    extract_desired_links(links).uniq
   end
 
-  def scrape(url)
-    reset
+  # 
+  # Given a page url, fetch out all outbound links from that page
+  # @param page [String] a /wiki/Page_URL
+  # 
+  # @return [Array] of /wiki/Linked_Pages
+  def scrape_page_for_links(page)
+    puts "Fetching #{WIKIPEDIA_URL + page}"
 
-    @url = url
+    page_dom = Nokogiri::HTML(fetch_html(WIKIPEDIA_URL + page))
 
-    page = Nokogiri::HTML(fetch_html(WIKIPEDIA_URL + url))
-
-    @links = page.css('a').map { |link| link['href'] }
+    page_dom.css('a').map { |link| link['href'] }
   end
 
   def fetch_html(url)
     req = HTTParty.get(url)
     return req.body
+  end
+
+  #
+  # Wikipedia uses relative links for everything we care about
+  # and has lots of other links we don't care about.
+  # Return only the subset of desired wiki pages
+  # 
+  # @param links [type] [description]
+  # 
+  # @return [type] [description]
+  def extract_desired_links(links)
+    links.reduce([]) do |subset, page| 
+      subset << page if whitelisted_page? page
+      subset
+    end
+  end
+
+  # 
+  # Verify that a given relative page URL is something that will
+  # lead us to bacon.
+  # 
+  # @param link [String] a /wiki/Page_Url_string
+  # 
+  # @return [Boolean]
+  def whitelisted_page?(page)
+    return false unless page
+    # pages with a : are media pages
+    return false if /:/.match page
+    # We only care about /wiki/ pages
+    page[0..5] == '/wiki/' ? true : false
   end
 
 private
--- spec/lib/scraper_spec.rb
+++ spec/lib/scraper_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Scraper do
+  let(:scraper) { Scraper.new }
+  
   describe 'extracting links from a page' do
-    let(:scraper) { Scraper.new }
-
     it 'loads the contents of each link into its internal property for access' do
       expect(scraper).to receive(:fetch_html).and_return("<strong>Some crap
         <a href='bob_and_tom'>whut</a>
@@ -9,13 +9,34 @@ RSpec.describe Scraper do
         </strong></i>
         <a prop='not_an_href'></a>")
 
-      scraper.scrape 'http://somelink.com'
-      expect(scraper.links).to eq ['bob_and_tom', 'the_morning_show', nil]
+      links = scraper.scrape_page_for_links 'http://somepage.com'
+      expect(links).to eq ['bob_and_tom', 'the_morning_show', nil]
 
       # Firing for a different page resets context
       expect(scraper).to receive(:fetch_html).and_return("")
-      scraper.scrape 'http://somelink.com'
-      expect(scraper.links).to eq []
+      links = scraper.scrape_page_for_links 'http://somelink.com'
+      expect(links).to eq []
+    end
+  end
+
+  describe 'checking if we care about a link with .whitelisted_link?' do
+    it 'rejects blank, empty, or media pages' do
+      expect(scraper.whitelisted_page?('')).to be false
+      expect(scraper.whitelisted_page?(nil)).to be false
+      expect(scraper.whitelisted_page?('/wiki/William_O%27Connell_Bradley#/media/File:William-O.-Bradley.jpg')).to be false
+    end
+
+    it 'allows wiki pages' do
+      expect(scraper.whitelisted_page?('/wiki/William_O%27Connell_Bradley')).to be true
+    end
+  end
+
+  describe '.find_unique_linked_pages' do
+    let(:unique_links) { scraper.find_unique_linked_pages('/wiki/William_O%27Connell_Bradley') }
+    
+    it 'returns an array of links with no duplicates' do
+      expect(unique_links.length).to eq(274)
+      expect(unique_links).to eq(unique_links.uniq)
     end
   end
 end
